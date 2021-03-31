3e819d8d810fde95df8409d0ef80a1aff16d027b
Kanellakopoulos Anastasios (HES) <anastasi.kanellak@hes.adhes.hesge.ch>
2021-03-31 21:24:01 +0200

    Major changes
    Added various translations and initiated coding the claculations for manual input

--- js/translation.js
+++ js/translation.js
@@ -1,4 +1,12 @@
 var french = {
-    "Sample":"Σαμπλέ",
-    "Empty Sample":"Εμπτύ Σαμπλέ",
+    "Sample quantity":"Quantité d'échantillon",
+    "No sample quantity":"Aucune quantité d'échantillon",
+    "Manual input": "Saisie manuelle",
+    "Input Files...": "Input Files...",
+    "Calculate": "Calculer",
+    "Back": "Retour",
+    "Region A": "Région A",
+    "Region B": "Région B",
+    "Region C": "Région C",
+    "Empty field": "Champ vide"
 };
